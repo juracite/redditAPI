bcd9b76ee707afd8c6b7e4d3af2127a6d761c246
Yann LM <yannlm2010@hotmail.fr>
2017-10-03 00:17:48 +0200

    Modified

--- app/funcs/index.js
+++ app/funcs/index.js
@@ -1,4 +1,6 @@
-var domains = [];
+const rp = require('request-promise');
+
+var domains;
 var funcs = [{
     func: "getAllRedditDomainsFromDate",
     description: "Get all domains from JSON using date",
@@ -8,23 +10,35 @@ var funcs = [{
      * @param {String} json The JSON
      * @return {String}
      */
-    execute: function(date, json) {
-        var json = JSON.parse(json);
-        var limit = Object.keys(json.data.children).length;
-        var todayDate = Date.now() / 1000 | 0;
+    execute: function(date, subreddit, pageAfter, callback) {
+        let req = 'https://www.reddit.com/r/' + subreddit + '.json?after=' + pageAfter + '&limit=100';
 
-        for (var i = 0; i < limit; i++) {
-            if (json.data.children[i].data.created >= date) {
-                var idPost = json.data.children[i].data.id;
-                var domain = json.data.children[i].data.domain;
-                var timestamp = json.data.children[i].data.created;
-                var dateTime = new Date(timestamp * 1000);
-                var dateTime = dateTime.getDay() + "/" + dateTime.getMonth() + "/" + dateTime.getFullYear();
+        rp(req)
+            .then(function(body) {
+                var jsonParsed = JSON.parse(body);
+                var limit = Object.keys(jsonParsed.data.children).length;
+                // var todayDate = Date.now() / 1000 | 0;
 
-                domains.push(idPost, domain, dateTime);
-            }
-        }
-        return domains;
+                let pageAfter = jsonParsed.data.after;
+                for (var i = 0; i < limit; i++) {
+                    if (jsonParsed.data.children[i].data.created >= date) {
+                        var idPost = jsonParsed.data.children[i].data.id;
+                        var domain = jsonParsed.data.children[i].data.domain;
+                        var timestamp = jsonParsed.data.children[i].data.created;
+                        var dateTime = new Date(timestamp * 1000);
+                        pageAfter = jsonParsed.data.after;
+                        var dateTime = dateTime.getDate() + "/" + dateTime.getMonth() + "/" + dateTime.getFullYear();
+
+                        domains = idPost + " - " + "Page : " + pageAfter + " - " + domain + " - " + dateTime;
+                        console.log(domains);
+                    }
+                }
+
+                return callback(domains, pageAfter, false);
+            })
+            .catch(function(err) {
+                return callback(false, false, err);
+            });
     }
 }];
 
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var rp = require('request-promise');
 const funcs = require('./app/funcs');
+let fromDate = 1504216800;
+let subreddit = 'programmation';
+//var domains = [];
 
-rp('https://www.reddit.com/r/programming.json')
-    .then(function(body) {
-        return funcs[0].execute(1504216800, body);
-    })
-    .then((res) => {
-        console.log(res);
-    })
-    .catch(function(err) {
-        console.log(err);
+function getAllPages(subreddit, pageAfter) {
+
+    funcs[0].execute(fromDate, subreddit, pageAfter, function(newDomains, newPageAfter, err) {
+        if (err) return console.log(err);
+        console.log(pageAfter);
+        domains = domains.concat(newDomains);
+
+        getAllPages(subreddit, newPageAfter);
     });
+
+};
+
+funcs[0].execute(fromDate, subreddit, '', function(newDomains, newPageAfter, err) {
+    if (err) return console.log(err);
+    domains = newDomains;
+    getAllPages(subreddit, newPageAfter);
+
+});
