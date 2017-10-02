f2c7953d1f2fd6a744cf194008e9eab898ad18f2
Yann LM <yannlm2010@hotmail.fr>
2017-10-03 01:45:43 +0200

    Fix, changes

--- app/funcs/index.js
+++ app/funcs/index.js
@@ -4,36 +4,36 @@ var domains = [];
 var funcs = [{
     func: "getAllRedditDomainsFromDate",
     description: "Get all domains from JSON using date",
-    parameters: ["date", "json"],
-    /**
-     * @param {array} date The date (timestamp) from where to get all data
-     * @param {String} json The JSON
-     * @return {String}
-     */
     execute: function(date, subreddit, pageAfter, callback) {
-        let req = 'https://www.reddit.com/r/' + subreddit + '.json?after=' + pageAfter + '&limit=100';
+        let req;
+        let newPageAfter;
 
+        if (pageAfter == '') {
+            req = 'https://www.reddit.com/r/' + subreddit + '.json';
+        } else {
+            req = 'https://www.reddit.com/r/' + subreddit + '.json?after=' + pageAfter;
+        }
         rp(req)
             .then(function(body) {
                 var jsonParsed = JSON.parse(body);
                 var limit = Object.keys(jsonParsed.data.children).length;
+                newPageAfter = jsonParsed.data.after;
                 // var todayDate = Date.now() / 1000 | 0;
-
-                let pageAfter = jsonParsed.data.after;
                 for (var i = 0; i < limit; i++) {
-                    if (jsonParsed.data.children[i].data.created >= date) {
+                    var postDate = new Date(jsonParsed.data.children[i].data.created * 1000);
+                    if (postDate < date) {
                         var idPost = jsonParsed.data.children[i].data.id;
                         var domain = jsonParsed.data.children[i].data.domain;
                         var timestamp = jsonParsed.data.children[i].data.created;
-                        var dateTime = new Date(timestamp * 1000);
-                        pageAfter = jsonParsed.data.after;
-                        var dateTime = dateTime.getDate() + "/" + dateTime.getMonth() + "/" + dateTime.getFullYear();
+                        var dateTime = postDate.getDate() + "/" + postDate.getMonth() + "/" + postDate.getFullYear();
 
                         domains.push(idPost + " - " + "Page : " + pageAfter + " - " + domain + " - " + dateTime);
                     }
                 }
-                console.log(domains);
-                return callback(domains, pageAfter, false);
+                return domains;
+            }).then((dom) => {
+                console.log(dom);
+                return callback(dom, newPageAfter, false);
             })
             .catch(function(err) {
                 return callback(false, false, err);
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const funcs = require('./app/funcs');
-let fromDate = 1504216800;
+let fromDate = new Date(1504216800 * 1000);
 let subreddit = 'programmation';
 var domains = [];
 
 function getAllPages(subreddit, pageAfter) {
-
-    funcs[0].execute(fromDate, subreddit, pageAfter, function(newDomains, newPageAfter, err) {
+    funcs[0].execute(fromDate, subreddit, pageAfter, async function(newDomains, newPageAfter, err) {
         if (err) return console.log(err);
-        domains = domains.concat(newDomains);
-
-        getAllPages(subreddit, newPageAfter);
+        domains = newDomains;
+        await getAllPages(subreddit, newPageAfter);
     });
-
 };
 
 getAllPages(subreddit, '');
